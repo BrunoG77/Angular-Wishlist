66746170dde8f236bb6366509f9349ea9798f99d
BrunoG77 <brunopintomonteiro99@gmail.com>
2025-02-25 18:52:32 +0000

    First Commit with some basic functionality
    For now it adds a wishlist and the user can tell
    if it has been fulfilled or not.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { wishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
+
+// To clean up the code
+const filters = [
+  (item : wishItem) => item,
+  (item : wishItem) => !item.isComplete,
+  (item : wishItem) => item.isComplete
+]
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [FormsModule]
 })
 export class AppComponent {
+  // create items array to contain various wish item objects
+  items : wishItem[] = [
+    new wishItem("Learn Angular"),
+    new wishItem("Finish God of War", true),
+    new wishItem("Get a job")
+  ];
   title = 'AngularTutWishlist';
+
+
+  addNewWish(newWish : string) {
+    if (newWish.trim()){
+      this.items.push(new wishItem(newWish.trim()))
+    }
+  }
+
+  // To Filter the list
+  listFilter : any = "0";
+
+  get visibleItems() : wishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  };
+
+  toggleItem(item : wishItem){
+    item.isComplete = !item.isComplete
+    console.log(item)
+  }
 }
---
+++ src/shared/models/wishItem.ts
@@ -0,0 +1,6 @@
+// Make a new class that has a constructor for a new Wish Item
+export class wishItem {
+    constructor(public wishText : string, public isComplete : boolean = false) {
+        
+    }
+}
